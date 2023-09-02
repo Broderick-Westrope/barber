53243d4c235d490a3f8151bd2cb77f69940c123f
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-02 16:58:14 +1000

    feat: add golangci-lint to post-create script; add golangci-lint config

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -1,4 +1,5 @@
 # aliases
 alias cls='clear'
 
-# 
+# Install golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2

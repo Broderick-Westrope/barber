01f15051180b33a218cb0b3a8ac41b0a4b3a5ff3
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-02 10:33:31 +1000

    feat: add collection remove command

--- cmd/collection_rm.go
+++ cmd/collection_rm.go
@@ -2,19 +2,50 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/Broderick-Westrope/barber/internal"
 	"github.com/spf13/cobra"
 )
 
 var collRmCmd = &cobra.Command{
 	Use:   "rm",
 	Short: "Remove a collection",
+	Long: `Remove a collection by removing the metadata file.
+			This will not remove the git repository.
+			If the metadata file does not exist, nothing will happen.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Implement
-		fmt.Println("Not yet implemented")
+		removeCollection()
 	},
 }
 
 func init() {
+	collRmCmd.PersistentFlags().BoolVarP(&skipConfirm, "yes", "y", false, "confirm removal without prompting")
+
 	collectionCmd.AddCommand(collRmCmd)
+}
+
+func removeCollection() {
+	if _, err := os.Stat(metadataFile); os.IsNotExist(err) {
+		fmt.Printf("'%s' file does not exist\n", metadataFile)
+		return
+	} else if err != nil {
+		log.Fatalf("Failed to check if '%s' file exists: %v", metadataFile, err)
+	} else {
+		if skipConfirm {
+			internal.RemoveFile(metadataFile)
+		} else {
+			fmt.Printf("Are you sure you want to remove '%s' file? [y/N] ", metadataFile)
+			var response string
+			if _, err := fmt.Scanln(&response); err != nil {
+				log.Fatalf("Failed to read user input: %v", err)
+			}
+			if response == "y" || response == "Y" {
+				internal.RemoveFile(metadataFile)
+			} else {
+				fmt.Printf("'%s' file was not removed\n", metadataFile)
+			}
+		}
+	}
 }

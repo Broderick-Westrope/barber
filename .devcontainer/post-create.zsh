d9ff34fdf903d351139ff4a993d3a4ba0dc86c40
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-02 17:30:11 +1000

    feat: add task config; add task installation to post-create script

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -3,3 +3,6 @@ alias cls='clear'
 
 # Install golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+
+# Install taskfile
+go install github.com/go-task/task/v3/cmd/task@latest

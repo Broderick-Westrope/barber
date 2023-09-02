da3d5c0436deed82e3e97627f4fb90c15fc5168e
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-02 11:18:05 +1000

    feat: improve collection init command

--- cmd/collection_init.go
+++ cmd/collection_init.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
 
+	"github.com/Broderick-Westrope/barber/internal"
 	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
@@ -12,6 +15,9 @@ import (
 var collInitCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new collection",
+	Long: `Initialize a new collection by creating a git repository and a metadata file.
+			If a git repository already exists, it will not be re-initialized.
+			If a metadata file already exists, it will not be re-created.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		initCollection()
 	},
@@ -38,34 +44,15 @@ func initCollection() {
 	}
 
 	// Check if metadata file exists, if not, create it
-	metadataFile := ".barber.yaml"
-	if _, err = os.Stat(metadataFile); os.IsNotExist(err) {
-		fmt.Printf("Creating a '%s' file...\n", metadataFile)
-		if err := createMetadataFile(metadataFile); err != nil {
+	if _, err = os.Stat(metadataFile); errors.Is(err, fs.ErrNotExist) {
+		fmt.Printf("Creating '%s' metadata file...\n", metadataFile)
+		if err := internal.CreateMetadataFile(metadataFile); err != nil {
 			log.Fatalf("Failed to create metadata file: %v", err)
 		}
 		fmt.Printf("Created '%s' file\n", metadataFile)
 	} else if err != nil {
 		log.Fatalf("Failed to check if '%s' file exists: %v", metadataFile, err)
 	} else {
-		fmt.Printf("'%s' file already exists. Resetting it...\n", metadataFile)
-		if err := createMetadataFile(metadataFile); err != nil {
-			log.Fatalf("Failed to reset '%s' file: %v", metadataFile, err)
-		}
-		fmt.Printf("Reset '%s' file\n", metadataFile)
+		fmt.Printf("'%s' file already exists\n", metadataFile)
 	}
-}
-
-func createMetadataFile(filename string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(`# Collection metadata`)
-	if err != nil {
-		return err
-	}
-	return nil
 }

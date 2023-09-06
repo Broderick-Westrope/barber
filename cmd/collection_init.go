f4bf88a302f4df4d956769dbd58333cab5e74a78
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-06 21:32:51 +1000

    feat: improve collection init command

--- cmd/collection_init.go
+++ cmd/collection_init.go
@@ -3,16 +3,15 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"io/fs"
 	"log"
-	"os"
+	"path/filepath"
 
 	"github.com/Broderick-Westrope/barber/internal"
 	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
 
-var collInitCmd = &cobra.Command{
+var colInitCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new collection",
 	Long: `Initialize a new collection by creating a git repository and a metadata file.
@@ -24,53 +23,47 @@ var collInitCmd = &cobra.Command{
 }
 
 func init() {
-	collectionCmd.AddCommand(collInitCmd)
+	collectionCmd.AddCommand(colInitCmd)
 }
 
+// Initializes a new collection by creating a git repository, a metadata file, and a config file.
+// If a git repository already exists, it will not be re-initialized.
+// If a file already exists, it will not be re-created.
 func initCollection() {
-	_, err := git.PlainOpen(".")
+	// TODO: Get path from collection flag
+	path := "."
+
+	// TODO: Provide a config option to disable git repository creation
+	if err := initGitRepo(path); err != nil {
+		log.Println(err)
+	}
+
+	metadataPath := filepath.Join(path, metadataFilename)
+	if err := internal.InitFile(metadataPath, internal.Metadata); err != nil {
+		log.Println(err)
+	}
+
+	configPath := filepath.Join(path, configFilename)
+	if err := internal.InitFile(configPath, internal.Config); err != nil {
+		log.Println(err)
+	}
+}
+
+// Checks if a git repository exists at the path, if not, it will initialize a new one.
+func initGitRepo(path string) error {
+	_, err := git.PlainOpen(path)
 	switch {
 	case errors.Is(err, git.ErrRepositoryNotExists):
 		fmt.Println("Initializing a new git repository...")
 		_, err = git.PlainInit(".", false)
 		if err != nil {
-			log.Fatalf("Failed to initialize git repository: %v", err)
+			return fmt.Errorf("Failed to initialize git repository: %w", err)
 		}
 		fmt.Println("Git repository initialized")
 	case err == nil:
 		fmt.Println("Git repository already exists")
 	default:
-		log.Fatalf("Failed to open git repository: %v", err)
+		return fmt.Errorf("Failed to open git repository: %w", err)
 	}
-
-	// Check if git repository exists, if not, initialize a new one
-	_, err = git.PlainOpen(".")
-	switch {
-	case errors.Is(err, git.ErrRepositoryNotExists):
-		fmt.Println("Initializing a new git repository...")
-		_, err = git.PlainInit(".", false)
-		if err != nil {
-			log.Fatalf("Failed to initialize git repository: %v", err)
-		}
-		fmt.Println("Git repository initialized")
-	case err == nil:
-		fmt.Println("Git repository already exists")
-	default:
-		log.Fatalf("Failed to open git repository: %v", err)
-	}
-
-	// Check if metadata file exists, if not, create it
-	_, err = os.Stat(defMetadataFile)
-	switch {
-	case errors.Is(err, fs.ErrNotExist):
-		fmt.Printf("Creating '%s' metadata file...\n", defMetadataFile)
-		if err = internal.CreateMetadataFile(defMetadataFile); err != nil {
-			log.Fatalf("Failed to create metadata file: %v", err)
-		}
-		fmt.Printf("Created '%s' file\n", defMetadataFile)
-	case err == nil:
-		fmt.Printf("'%s' file already exists\n", defMetadataFile)
-	default:
-		log.Fatalf("Failed to check if '%s' file exists: %v", defMetadataFile, err)
-	}
+	return nil
 }

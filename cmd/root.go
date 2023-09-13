85cffdc422588cf89f1c5f00d244f0298dcf160c
Broderick Westrope <broderickwestrope@gmail.com>
2023-09-13 15:18:31 +1000

    docs(help): fix formatting for root command long message

--- cmd/root.go
+++ cmd/root.go
@@ -22,8 +22,8 @@ var rootCmd = &cobra.Command{
 	Use:   "barber",
 	Short: "Barber is a tool for managing snippets",
 	Long: `Barber is a CLI & TUI for managing snippets.
-			It is targeted towards code snippets, but can be used for any text format.
-			Documentation is available at https://github.com/Broderick-Westrope/barber`,
+It is targeted towards code snippets, but can be used for any text format.
+Documentation is available at https://github.com/Broderick-Westrope/barber`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Launch interactive mode
 		fmt.Println("Interactive mode not yet implemented")
